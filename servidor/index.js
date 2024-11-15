93b98c22c62ee0b254fa8948d7d50a1359cc2ecc
franciscoannoni <franciscoannoni@gmail.com>
2024-11-14 22:40:12 -0300

    hago coneccion con socket.io, y agrego colores para cada usuario individual

--- servidor/index.js
+++ servidor/index.js
@@ -4,21 +4,48 @@ import logger from "morgan";
 import {Server} from 'socket.io'
 import {createServer} from 'node:http' 
 
-
 const port = process.env.PORT ?? 3000
 
 const app = express()
 
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+    connectionStateRecovery: {
+        maxDisconnectionDuration: 5000,
+        skipMiddlewares:true
+    }
+
+})
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
 
 io.on('connection', (socket) =>
 {
+    const userColor = getRandomColor();
+    console.log('-------------------------------')
+    console.log('================================')
     console.log('a user has connected!')
 
     socket.on('disconnect', () => {
         console.log('an user has disconnected')
     })
+    console.log('================================')
+ 
+   
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', { text: msg, color: userColor });
+        console.log('message: ' + msg);
+      });
+ 
+
 })
 
 app.use(logger('dev'))
@@ -28,5 +55,5 @@ app.get('/',(req,res)=>{
 })
 
 server.listen(port, () => {
-    console.log('server esta activo y escuchando en puerto ${port}')
+    console.log(`server esta activo y escuchando en puerto *:${port}`)
 })
