1b7d7667fc7973f815019ea3f3ef0734971020f2
franciscoannoni <franciscoannoni@gmail.com>
2024-11-15 01:43:09 -0300

    agrego nueva dependencia para conectarme a una base de datos

--- servidor/index.js
+++ servidor/index.js
@@ -1,32 +1,48 @@
 import express from "express";
 import logger from "morgan";
+import dotenv from 'dotenv'
 
+
+import {createClient} from '@libsql/client'
 import {Server} from 'socket.io'
 import {createServer} from 'node:http' 
 
+
+//Utilizacion del archivo .nev
+dotenv.config()
+
+//Declaracion del puerto
 const port = process.env.PORT ?? 3000
 
+//Declaracion de la App
 const app = express()
 
+//Declaracion del servidor que utiliza la app
 const server = createServer(app)
+
+//Declaracion del io que es vasicamente el Socket que le manda a todo el mundo
 const io = new Server(server, {
     connectionStateRecovery: {
         maxDisconnectionDuration: 5000,
         skipMiddlewares:true
     }
-
 })
 
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
-
+//Conexion con la Base de Datos
+const db = createClient({
+    url:"libsql://still-multiple-man-franciscoannoni.turso.io",
+    authToken: process.env.DB_TOKEN
+})
+// esto no m e funca
+/*
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        content TEXT
+        )
+    `)
+*/
+    
 io.on('connection', (socket) =>
 {
     const userColor = getRandomColor();
@@ -39,16 +55,14 @@ io.on('connection', (socket) =>
     })
     console.log('================================')
  
-   
     socket.on('chat message', (msg) => {
         io.emit('chat message', { text: msg, color: userColor });
         console.log('message: ' + msg);
       });
  
-
 })
 
-app.use(logger('dev'))
+app.use(logger('dev')) //Una extencion que nos logea las cosas al pedo pero sirve para codear
 
 app.get('/',(req,res)=>{
     res.sendFile(process.cwd() + '/cliente/index.html')
@@ -57,3 +71,14 @@ app.get('/',(req,res)=>{
 server.listen(port, () => {
     console.log(`server esta activo y escuchando en puerto *:${port}`)
 })
+
+
+// Funcion que utilizo para crear los colores claros para el Chat
+function getRandomColor() {
+    const letters = 'CDEF'; // Rango de letras para colores claros
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * letters.length)];
+    }
+    return color;
+  }
